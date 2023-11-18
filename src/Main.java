efd5990b203aa6c3a488348935418686f26340cd
Ayleen Roa <Roa.ayleen07@gmail.com>
2023-11-18 16:45:07 -0500

    Finished converter methods for txt and csv

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,14 @@ import java.io.PrintWriter;
 import java.util.*;
 public class Main {
     private static final String INPUT = System.getProperty("user.dir") + "/src/";
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
 
         System.out.println("Hello!");
 
-        while(true) {
+        while (true) {
             System.out.println();
 
             System.out.printf("--------------------------------------------------------------------------%n");
@@ -33,26 +34,87 @@ public class Main {
             String option = scan.next();
             String[] conversion = option.split(" ");
 
-            if (conversion.length != 3 || conversion.length !=2){
+            if (conversion.length != 3 || conversion.length != 2) {
                 System.out.println("Error invalid input please try again!");
             } else if (option.matches("quit")) {
                 break;
 
-            }else if (conversion[0].equals("convert")) {
+            } else if (conversion[0].equals("convert")) {
                 if (!conversion[1].endsWith(".txt") && !conversion[1].endsWith(".csv") || !conversion[2].endsWith(".txt") && !conversion[2].endsWith(".csv")) {
                     System.out.println("Error! The convert command needs to be source.xxx destination.yyy where xxx and yyy is csv or txt Please try again!");
                     continue;
                 }
-            }else if (conversion[0].equals("normalize")){
-                if (!conversion[1].endsWith(".txt") && !conversion[1].endsWith(".csv")){
+            } else if (conversion[0].equals("normalize")) {
+                if (!conversion[1].endsWith(".txt") && !conversion[1].endsWith(".csv")) {
                     System.out.println("Error The source command needs to be source.xxx where xxx is txt or csv");
                     continue;
                 }
             }
 
-
+        }
 
     }
+
+    public static void converter(String start, String convert) throws Exception {
+        String startEnd = start.substring(start.length() - 3);
+        String convertEnd = convert.substring(convert.length() - 3);
+        if (start.matches(convert)) {
+            throw new Exception("Same file");
+        }
+        Scanner in = new Scanner(new File(start));
+        PrintWriter out = new PrintWriter(convert);
+        if (startEnd.matches(convertEnd)){ // Same format
+            if (startEnd.matches("txt")){ // text to txt
+                while(in.hasNextLine()){
+                    String [] text = in.nextLine().split("\t");
+                    for (int i = 0; i < text.length; i++){
+                        String seperate = "\t";
+                        if (i == text.length -1){
+                            if(in.hasNextLine())
+                                seperate = "\n";
+                            else
+                                seperate = "";
+                        }
+                        out.print(text[i] + seperate);
+                    }
+                    out.flush();
+                }
+            } else { // csv to csv
+                while (in.hasNextLine()){
+                    String input = in.nextLine();
+                    String[] cells = input.split(",");
+                    for (int i = 0; i < cells.length; i++){
+                        String seperate = ",";
+                        if (i == cells.length -1 ){
+                            seperate = "";
+                        }
+                        out.print(cells[i] + seperate);
+                    }
+                    out.println();
+                }
+
+            }
+        }else {
+            if (startEnd.matches("txt")){   // txt to csv
+                while (in.hasNextLine()){
+                    String input = in.nextLine();
+                    String [] text = input.split(",");
+                    for (int i = 0; i < text.length; i++){
+                        String seperate = "\t";
+                        if (i == text.length -1){
+                            seperate = "";
+                        }
+                        out.print(text[i] + seperate);
+                    }
+                    out.println();
+                }
+
+
+            }
+        }
+        out.close();
+        in.close();
+
     }
 
 }
